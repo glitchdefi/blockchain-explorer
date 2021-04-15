187b76cdd4746d25a45e72a09aa9740db06c920a
minggltichfe <ming@glitch.finance>
2021-04-15 17:54:11 +0700

    Fix css bg

--- src/app/layouts/Container.js
+++ src/app/layouts/Container.js
@@ -4,7 +4,7 @@ import bg from "./assets/g_bg.jpg";
 export const Container = styled.div(() => [
   css`
     background-image: url(${bg});
-    background-position: center;
+    background-position: top center;
     background-repeat: no-repeat;
     background-size: 100% auto;
     background-attachment: fixed;
