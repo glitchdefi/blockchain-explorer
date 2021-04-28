dd87bb754e0e60abbb9c1f8c29091606143d6bff
minggltichfe <ming@glitch.finance>
2021-04-28 14:04:53 +0700

    feat: create token page

---
+++ src/app/pages/TokenPage/components/Header.js
@@ -0,0 +1,23 @@
+import React from "react";
+import tw from "twin.macro";
+
+import { Text } from "src/app/components/Text";
+import { Button } from "src/app/components/Button";
+
+export function Header() {
+  return (
+    <Wrapper>
+      <div tw="flex items-center">
+        <Text tw="text-16">Selected Producers</Text>
+        <Text tw="text-16 text-white-70! ml-3">0/30</Text>
+      </div>
+
+      <VoteButton>
+        <Text tw="font-bold uppercase">VOTE</Text>
+      </VoteButton>
+    </Wrapper>
+  );
+}
+
+const Wrapper = tw.div`flex flex-wrap justify-between items-center`;
+const VoteButton = tw(Button)`bg-gradient-to-r from-primary to-secondary px-8 py-2`;
